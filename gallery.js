36dc28480e1ab8d42ccfcbc3c954818f1c0cc4c9
beepboop617 <bibygm617@gmail.com>
2026-02-07 18:03:08 -0600

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,1 +1,15 @@
+document.querySelectorAll(".work").forEach(work => {
+  work.addEventListener("click", () => {
+    let caption = work.querySelector(".caption");
+
+    if (!caption) {
+      caption = document.createElement("div");
+      caption.className = "caption";
+      caption.innerHTML = work.dataset.caption;
+      work.appendChild(caption);
+    } else {
+      caption.remove();
+    }
+  });
+});
 
